27ca1b68b9d98366fec4de70e68755fcf6301aaa
flyingwater <flyingwater@163.com>
2023-11-07 19:54:38 +0800

    finish

--- experiment.py
+++ experiment.py
@@ -56,6 +56,7 @@ def process(matchings_path):
 
 
 def approx(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set, ans_list):
+    print("start approx~")
     approx_h_list = [ex_fact.compute_entropy()]
     time_list = []
     cost_list = []
@@ -79,12 +80,13 @@ def approx(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set, an
         tmp_cost = 0
         for ix in selection_idxes:
             tmp_cost += ex_fact.len_list()[ix]
-            
-        cost_list.append(tmp_cost)
+        cost_list.append(int(tmp_cost))
+        
     return approx_h_list, time_list, cost_list, list(ex_fact.get_prior_p())
 
 
 def brute(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set, ans_list):
+    print("start brute~")
     brute_h_list = [ex_fact.compute_entropy()]
     time_list = []
     cost_list = []
@@ -109,10 +111,11 @@ def brute(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set, ans
         for ix in selection_idxes:
             tmp_cost += ex_fact.len_list()[ix]
             
-        cost_list.append(tmp_cost)
+        cost_list.append(int(tmp_cost))
     return brute_h_list, time_list, cost_list, list(ex_fact.get_prior_p())
 
 def heuristic(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set, ans_list):
+    print("start heuristic~")
     heuristic_h_list = [ex_fact.compute_entropy()]
     time_list = []
     cost_list = []
@@ -137,14 +140,15 @@ def heuristic(ex_fact, query_selector, budget, acc, total_turns, api_use, c_set,
         for ix in selection_idxes:
             tmp_cost += ex_fact.len_list()[ix]
             
-        cost_list.append(tmp_cost)
+        cost_list.append(int(tmp_cost))
     return heuristic_h_list, time_list, cost_list, list(ex_fact.get_prior_p())
 
 def random_algorithm(random_fact, query_selector, budget, acc, total_turns, api_use, c_set, ans_list):
+    print("start random~")
     all_h_list = []
     prior_p_l = []
     for _ in range(100):
-        random_i_fact = deep.copy(random_fact)
+        random_i_fact = copy.deepcopy(random_fact)
         cost_sum_r = 0
         turns_r=total_turns
         random_h_list=[random_i_fact.compute_entropy()]
@@ -155,7 +159,7 @@ def random_algorithm(random_fact, query_selector, budget, acc, total_turns, api_
             else:
                 ans_r = [1 if ans_list[ix_r]=="yes" else 0 for ix_r in random_selection_idxes]
             p_a_r,p_a_v_r = random_i_fact.compute_ans_p(ans_r, random_selection_idxes, acc)
-            p_post = random_i_fact.get_prior_p()*p_a_v / p_a
+            p_post = random_i_fact.get_prior_p()*p_a_v_r / p_a_r
             random_i_fact.set_prior_p(p_post)
             random_h_list.append(random_i_fact.compute_entropy())
             turns_r -=1
@@ -189,15 +193,42 @@ def save_h_file(approx_name_f, random_f, brute_name_f, heuristic_name_f, approx_
         
     with open(f"./{dataset}/"+ heuristic_name_f, "w") as f3:
         json.dump(heuristic_ob, f3, indent=2, ensure_ascii=False)
-        
-
-approx = {"entropy":approx_h_list, "timecost":approx_timecost, "prob":list(ex_fact.get_prior_p())}
-random = {"entropy":random_h_list, "timecost":0, "prob":list(random_fact.get_prior_p())}
-brute = {"entropy":brute_h_list, "timecost":brute_timecost, "prob":list(brute_fact.get_prior_p())}
-heuristic = {"entropy":heuristic_h_list, "timecost":heuristic_timecost, "prob":list(heuristic_fact.get_prior_p())}
 
 if __name__ == "__main__":
-    budgets = [20, 25, 30, 35]
+    budgets = [15, 20, 25, 30]
     
     total_turns = 10
+    names = ["author", "employee", "purchase"]
+    p_w = [0.89, 0.92, 0.87] 
+    paths = [f"./dataset/{i}.json" for i in ["author", "employee", "purchase"]]
+    ans_paths = [f"./dataset/{i}_ans.json" for i in ["author", "employee", "purchase"]]
     
+    for dataset_id, path_i in enumerate(paths):
+        for budget in budgets:
+            fact_selectors, c_num, c_set = process(path_i)
+        
+            acc = np.array([[p_w[dataset_id] for i in range(c_num)]])
+            api_use = False
+            ans_list = answers(ans_paths[dataset_id])
+        
+            approx_fact, approx_selector = fact_selectors[0]
+            approx_h, approx_time, approx_cost, approx_post_p = approx(approx_fact, approx_selector, budget, acc, total_turns, api_use, c_set, ans_list)
+            
+            random_fact, random_selector = fact_selectors[1]
+            random_h, random_post_p = random_algorithm(random_fact, random_selector, budget, acc, total_turns, api_use, c_set, ans_list)
+            
+            brute_fact, brute_selector = fact_selectors[2]
+            brute_h, brute_time, brute_cost, brute_post_p = brute(brute_fact, brute_selector, budget, acc, total_turns, api_use, c_set, ans_list)
+            
+            h_fact, h_selector = fact_selectors[3]
+            heuristic_h, heuristic_time, heuristic_cost, heuristic_post_p = heuristic(h_fact, h_selector, budget, acc, total_turns, api_use, c_set, ans_list)
+            
+            approx_result = {"entropy":approx_h, "timecost":approx_time,"cost": approx_cost, "prob":approx_post_p}
+            print(type(approx_h)), print(type(approx_time)), print(type(approx_cost)), print(type(approx_post_p))
+            random_result = {"entropy":random_h, "prob":random_post_p}
+            brute_result = {"entropy":brute_h, "timecost":brute_time,"cost":brute_cost ,"prob":brute_post_p}
+            heuristic_result = {"entropy":heuristic_h, "timecost":heuristic_time, "cost":heuristic_cost,"prob":heuristic_post_p}
+            
+            
+            save_h_file(f"{names[dataset_id]}_approx_{budget}.json", f"{names[dataset_id]}_random_{budget}.json", f"{names[dataset_id]}_brute_{budget}.json", f"{names[dataset_id]}_heuristic_{budget}.json", approx_result, random_result, brute_result, heuristic_result)
+            
